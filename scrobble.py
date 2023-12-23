3633ce9cf37060a8bc06ea8b9c6b9604d8378379
Burrito <37681398+burritosoftware@users.noreply.github.com>
2023-12-22 21:37:16 -0800

    Album support (this should really be seperated)

--- scrobble.py
+++ scrobble.py
@@ -1,3 +1,4 @@
+import pylast
 import managers.lastfm
 import managers.icecast
 import time
@@ -9,41 +10,56 @@ LASTFM_NETWORK = managers.lastfm.get_network()
 ICECAST_SOURCE = int(os.getenv("ICECAST_SOURCE"))
 UPDATE_FREQUENCY = int(os.getenv("UPDATE_FREQUENCY"))
 
-current_details = ""
+current_details = {}
+last_details = {}
+
 # This should really not be a forever loop and should be cleaner later
 while True:
-  timestamp=int(time.time())
+  # Get the current song and associated details
   source = managers.icecast.get_source(ICECAST_SOURCE)
   details = source['title']
   artist, song = details.rsplit(" - ", 1)
-
-  # Clean the artist variable by getting the first artist
   artist = artist.split(",")[0]
+  remove_tags = ["(Original Mix)", "[FNT Edit]"]
+  for tag in remove_tags:
+    song = song.replace(tag, "").strip()
   
-  # Clean the song variable by removing (Original Mix) if it exists
-  song = song.replace("(Original Mix)", "").strip()
-  song = song.replace("[FNT Edit]", "").strip()
+  current_details['artist'] = artist
+  current_details['song'] = song
+  
+  # Update the current now playing every update (songs can be long) (only if album key does not exist and isn't pylast.WSError)
+  if 'album' not in current_details:
+    try:
+      current_details['album'] = LASTFM_NETWORK.get_track(artist=artist, title=song).get_album().get_title()
+      print(f"Retrieved album for {song}: {current_details['album']}")
+    except:
+      current_details['album'] = "icecast-scrobbler.albumnotfound"
+      print(f"Couldn't find album: {song}")
 
-  # Update the current now playing every update (songs can be long)
-  LASTFM_NETWORK.update_now_playing(
-      artist=artist,
-      title=song
-  )
+  if current_details['album'] != "icecast-scrobbler.albumnotfound":
+    LASTFM_NETWORK.update_now_playing(
+        artist=artist,
+        title=song,
+        album=current_details['album']
+    )
+  else:
+    LASTFM_NETWORK.update_now_playing(
+        artist=artist,
+        title=song
+    )
 
   # If the song changed, update Last.fm
-  if details != current_details:
-    print("\nOop, new change!")
+  if last_details != current_details:
     # Update the current now playing
-    last_details = current_details
-    current_details = details
-    print(f"Updated now playing to {artist} - {song}")
+    print(f"Updated now playing: {artist} - {song}")
 
     # Scrobble the last song if there was a last song
-    if last_details != "":
-      last_artist, last_song = last_details.rsplit(" - ", 1)
-      last_artist = last_artist.split(",")[0]
-      last_song = last_song.replace("(Original Mix)", "").strip()
-      last_song = last_song.replace("[FNT Edit]", "").strip()
-      managers.lastfm.scrobble(last_artist, last_song, timestamp)
-      print(f"Scrobbled {last_artist} - {last_song}")
+    if last_details != {}:
+      timestamp=int(time.time())
+      managers.lastfm.scrobble(artist=last_details['artist'], song=last_details['song'], timestamp=timestamp)
+      print(f"Scrobbled: {last_details['artist']} - {last_details['song']}")
+
+    last_details = current_details
+    print()
+  
   time.sleep(UPDATE_FREQUENCY)
